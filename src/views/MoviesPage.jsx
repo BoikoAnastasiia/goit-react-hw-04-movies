f4eb5c289054d4dc5449ca6c1e37246c571358d1
anastasyah86 <nastasyah86@gmail.com>
2021-04-15 14:39:45 +0300

    undefined

--- src/views/MoviesPage.jsx
+++ src/views/MoviesPage.jsx
@@ -1,37 +1,53 @@
 import Searchbar from '../components/Searchbar';
 import React, { Component } from 'react';
+import searhForMovies from '../services/searhForMovies-api';
 
 class MoviesPage extends Component {
   state = {
     query: '',
-    id: null,
-    poster_path: null,
-    vote_count: null,
-    title: null,
-    release_date: null,
-    overview: null,
-    vote_average: 0,
+    movies: [],
   };
 
   onChangeQuery = query => {
     this.setState({
       query: query,
-      id: null,
-      poster_path: null,
-      vote_count: null,
-      title: null,
-      release_date: null,
-      overview: null,
-      vote_average: 0,
+      movies: [],
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchMovies();
+    }
+  }
+
+  fetchMovies = () => {
+    const { query } = this.state;
+    searhForMovies(query).then(results => {
+      this.setState({ movies: results });
     });
   };
 
   render() {
+    const { movies } = this.state;
+    const baseUrl = 'https://image.tmdb.org/t/p/w500';
     return (
       <div className="moviesContainer">
         <Searchbar onSubmit={this.onChangeQuery} />
-        <ul>
-          <li></li>
+        <ul className="homepageMoviesList">
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <img
+                src={baseUrl + movie.poster_path}
+                className="ImageGalleryItem-image"
+                alt={movie.title}
+              />
+              <h2 className="homepageMovieTitle">{movie.title}</h2>
+              <span>
+                ⭐️ <span className="rate">{movie.vote_average}</span>
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     );
