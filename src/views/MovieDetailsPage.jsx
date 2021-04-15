faab7d222f5e8a28068c59771162b020f32661a9
anastasyah86 <nastasyah86@gmail.com>
2021-04-15 13:23:21 +0300

    search bar styles

--- src/views/MovieDetailsPage.jsx
+++ src/views/MovieDetailsPage.jsx
@@ -5,17 +5,6 @@ import movieApi from '../services/single-movie-api';
 import { apiKey } from '../services/apiKey';
 
 class MovieDetailsPage extends Component {
-  state = {
-    query: '',
-    id: null,
-    poster_path: null,
-    vote_count: null,
-    title: null,
-    release_date: null,
-    overview: null,
-    vote_average: 0,
-  };
-
   async componentDidMount() {
     const response = await axios.get(
       `https://api.themoviedb.org/3/movie/${this.state.id}?${apiKey}`,
@@ -23,19 +12,6 @@ class MovieDetailsPage extends Component {
     console.log(response.data);
     this.setState({ ...response.data });
   }
-
-  onChangeQuery = query => {
-    this.setState({
-      query: query,
-      id: null,
-      poster_path: null,
-      vote_count: null,
-      title: null,
-      release_date: null,
-      overview: null,
-      vote_average: 0,
-    });
-  };
 
   render() {
     const {
@@ -48,7 +24,7 @@ class MovieDetailsPage extends Component {
     } = this.state;
 
     return (
-      <>
+      <div className="moviesContainer">
         <h1> {this.props.match.params.movieId} </h1>
         <SearchBar onSubmit={this.onChangeQuery} />
         <span>Views {vote_count} </span>
@@ -57,7 +33,7 @@ class MovieDetailsPage extends Component {
         <h1> {title}</h1>
         <img src={poster_path} alt={title} />
         <p>{overview}</p>
-      </>
+      </div>
     );
   }
 }
--- src/views/MoviesPage.jsx
+++ src/views/MoviesPage.jsx
@@ -2,15 +2,38 @@ import Searchbar from '../components/Searchbar';
 import React, { Component } from 'react';
 
 class MoviesPage extends Component {
-  state = { query: '' };
+  state = {
+    query: '',
+    id: null,
+    poster_path: null,
+    vote_count: null,
+    title: null,
+    release_date: null,
+    overview: null,
+    vote_average: 0,
+  };
+
+  onChangeQuery = query => {
+    this.setState({
+      query: query,
+      id: null,
+      poster_path: null,
+      vote_count: null,
+      title: null,
+      release_date: null,
+      overview: null,
+      vote_average: 0,
+    });
+  };
+
   render() {
     return (
-      <>
-        <Searchbar></Searchbar>
+      <div className="moviesContainer">
+        <Searchbar onSubmit={this.onChangeQuery} />
         <ul>
           <li></li>
         </ul>
-      </>
+      </div>
     );
   }
 }
